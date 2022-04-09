b5c3ba076d4d574072837c3ee9775b9075f2702f
Kostja P <kpavlovsky@users.noreply.github.com>
2022-04-09 09:20:08 +0200

    added models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class TaskList(models.Model):
+    name = models.CharField(max_length=255)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Task List'
+        verbose_name_plural = 'Task Lists'
+        ordering = ('-id', )
+
+
+class Task(models.Model):
+    task_list = models.ForeignKey(TaskList, on_delete=models.CASCADE)
+    title = models.TextField()
+    is_done = models.BooleanField(default=False, db_index=True)
+    created_dt = models.DateTimeField(auto_now_add=True, db_index=True)
+    updated_dt = models.DateTimeField(auto_now=True, db_index=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Task'
+        verbose_name_plural = 'Tasks'
+        ordering = ('-id', )
